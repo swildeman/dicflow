dd4b6d08ca0e985d2508bc9e62efe23c5f9af4ab
swildeman <swildeman@localhost>
2018-05-05 20:20:15 +0200

    improved boundary handling, new options: fix boundary using 'mirror' approach; assume finite difference gradients instead of analytical gradients

--- general/fftinvgrad.m
+++ general/fftinvgrad.m
@@ -1,13 +1,17 @@
-function f = fftinvgrad( fx, fy, impbc )
+function f = fftinvgrad( fx, fy, varargin )
 % FFTINVGRAD Integrate a gradient field (up to a constant) using FFT2
 % 
-% SYNOPSIS: f = fftinvgrad( fx, fy, impbc )
+% SYNOPSIS: f = fftinvgrad( fx, fy, (opt) 'bcFix', ..., (opt) 'gradType', ... )
 %
 % INPUT fx: gradient accross columns (left to right)
 %       fy: gradient accross rows (up to down)
-%       impbc: (default: true) add a special impulse function on the 
+%       'bcFix', {'impulse', 'mirror', 'none'}: (default: impulse) 
+%              add a special impulse function on the 
 %              boundaries of the domain to correct for periodicity assumed 
-%              in FFT 
+%              in FFT
+%       'gradType', {'spectral', 'difference'}: (default: 'spectral')
+%              when 'difference' is specified, [fx,fy] is assumed to come 
+%              from a central difference gradient, instead of being analytical
 %
 % OUTPUT f: integrated gradient field such that [fx,fy] = grad(f) in a
 %           least squares sense
@@ -24,17 +28,49 @@ function f = fftinvgrad( fx, fy, impbc )
 % Copyright (c) 2017 Sander Wildeman
 % Distributed under the MIT License, see LICENSE file
 
+% -- Parse input options -- %
+defaultBcFix = 'impulse';
+expectBcFix = {'impulse', 'mirror', 'none'};
 
-if nargin < 3
-   impbc = true; 
+defaultGradType = 'spectral';
+expectGradType = {'spectral', 'difference'};
+
+p = inputParser;
+p.addParameter('bcfix', defaultBcFix,...
+    @(x) any(validatestring(x,expectBcFix)));
+p.addParameter('gradtype', defaultGradType, ...
+    @(x) any(validatestring(x,expectGradType)));
+p.parse(varargin{:});
+
+impbc = false;
+mirbc = false;
+switch validatestring(p.Results.bcfix, expectBcFix)
+    case 'impulse'
+        impbc = true;
+    case 'mirror'
+        mirbc = true;
 end
 
+finitediff = false;
+switch validatestring(p.Results.gradtype, expectGradType)
+    case 'difference'
+        finitediff = true;
+end
+
+if mirbc
+   fx = [fx, -fliplr(fx);  flipud(fx), -rot90(fx,2)];
+   fy = [fy,  fliplr(fy); -flipud(fy), -rot90(fy,2)];
+end
+
+% -- Integration algorithm -- %
 [Ny, Nx] = size(fx);
 
 if impbc
     % add impulse to boundaries to compensate for non-periodicity
     impIx = -.5*sum(fx(:,2:end-1),2);
     impIy = -.5*sum(fy(2:end-1,:),1);
+    fx_edge = fx(:,[1,end]); % save for later recovery
+    fy_edge = fy([1,end],:);
     fx(:,[1,end]) = [impIx, impIx];
     fy([1,end],:) = [impIy; impIy];
 end
@@ -45,7 +81,11 @@ mx = mean(fx(:));
 my = mean(fy(:));
 
 % generate k vectors according to matlab's fft layout
-[kx, ky] = meshgrid(kvec(Nx),kvec(Ny));
+if finitediff
+    [kx, ky] = meshgrid( sin(kvec(Nx)), sin(kvec(Ny)) );
+else
+    [kx, ky] = meshgrid( kvec(Nx), kvec(Ny));
+end
 % pre-compute k^2;
 k2 = kx.^2 + ky.^2;
 
@@ -62,7 +102,7 @@ fx_hat = fft2(fx);
 fy_hat = fft2(fy);
 
 % integrate in fourier domain
-k2(1,1) = 1; % shortcut to prevent division by zero (this effectively subtracts a linear plane)
+k2(k2 < eps) = 1; % shortcut to prevent division by zero (this effectively subtracts a linear plane)
 f_hat = (-1i*kx.*fx_hat + -1i*ky.*fy_hat)./k2;
 
 % transform back to spatial domain
@@ -71,6 +111,16 @@ f = real(ifft2(f_hat));
 % add mean slope back on
 [x,y] = meshgrid(0:Nx-1,0:Ny-1);
 f = f + mx*x + my*y;
+
+if impbc % fix edges
+    % use inverted second order difference at edges
+    f(:,1) = (4*f(:,2) - f(:,3) - 2*fx_edge(:,1)) / 3;
+    f(:,end) = (4*f(:,end-1) - f(:,end-2) + 2*fx_edge(:,2)) / 3;
+    f(1,:) = (4*f(2,:) - f(3,:) - 2*fy_edge(1,:)) / 3;
+    f(end,:) = (4*f(end-1,:) - f(end-2,:) + 2*fy_edge(2,:)) / 3;
+elseif mirbc
+    f = f(1:Ny/2,1:Nx/2);
+end
 
 end
 
